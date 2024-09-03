664b53e916ec11a15c343db9dfe1ff6fb9e8a81c
Marcel Bialojahn <marcel.bialojahn@cubos.com>
2024-09-03 17:03:59 +0200

    Redistributing functions to own files for readability

--- server/mqtt_config_helper.go
+++ server/mqtt_config_helper.go
@@ -1,1 +1,52 @@
 package server
+
+import (
+	"errors"
+
+	"github.com/evcc-io/evcc/util/config"
+	"github.com/evcc-io/evcc/util/templates"
+)
+
+func CubosIdToId(cubosId string, class templates.Class) (int, error) {
+	var id int
+	var err error
+	var res []map[string]any
+	switch class {
+	case templates.Meter:
+		if res, err = devicesConfig(class, config.Meters()); err != nil {
+			return 0, err
+		}
+
+	case templates.Charger:
+		if res, err = devicesConfig(class, config.Chargers()); err != nil {
+			return 0, err
+		}
+
+	case templates.Vehicle:
+		if res, err = devicesConfig(class, config.Vehicles()); err != nil {
+			return 0, err
+		}
+
+	case templates.Circuit:
+		if res, err = devicesConfig(class, config.Circuits()); err != nil {
+			return 0, err
+		}
+
+	case templates.Loadpoint:
+		if res, err = devicesConfig(class, config.Loadpoints()); err != nil {
+			return 0, err
+		}
+	}
+
+	for _, res2 := range res {
+		if res3, found := res2["config"].(map[string]any); found {
+			if cubos_id_, found2 := res3["cubosId"].(string); found2 {
+				if cubos_id_ == cubosId {
+					id = res2["id"].(int)
+					return id, nil
+				}
+			}
+		}
+	}
+	return 0, errors.New("No ID found for cubosId:" + cubosId)
+}
--- server/mqtt_config_loadpoint_handler.go
+++ server/mqtt_config_loadpoint_handler.go
@@ -1,1 +1,75 @@
 package server
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/evcc-io/evcc/core"
+	"github.com/evcc-io/evcc/core/loadpoint"
+	coresettings "github.com/evcc-io/evcc/core/settings"
+	"github.com/evcc-io/evcc/util"
+	"github.com/evcc-io/evcc/util/config"
+	"github.com/evcc-io/evcc/util/templates"
+	"github.com/samber/lo"
+)
+
+func MQTTnewLoadpointHandler(payload string) error {
+	h := config.Loadpoints()
+	msg := strings.NewReader(payload)
+	dynamic, static, err := loadpointSplitConfig(msg)
+
+	if err != nil {
+		return err
+	}
+
+	id := len(h.Devices())
+	name := "lp-" + strconv.Itoa(id+1)
+
+	log := util.NewLoggerWithLoadpoint(name, id+1)
+
+	dev := config.BlankConfigurableDevice[loadpoint.API]()
+	settings := coresettings.NewDeviceSettingsAdapter(dev)
+
+	instance, err := core.NewLoadpointFromConfig(log, settings, static)
+	if err != nil {
+		return err
+	}
+	_, err = config.AddConfig(templates.Loadpoint, "", static)
+	if err != nil {
+		return err
+	}
+	dev.Update(static, instance)
+	if err := h.Add(dev); err != nil {
+		return err
+	}
+
+	if err := loadpointUpdateDynamicConfig(dynamic, instance); err != nil {
+		return err
+	}
+
+	setConfigDirty()
+
+	return err
+}
+
+func MQTTdeleteLoadpointHandler(id int) error {
+	h := config.Loadpoints()
+
+	res := lo.Map(config.Loadpoints().Devices(), func(dev config.Device[loadpoint.API], _ int) loadpointFullConfig {
+		return loadpointConfig(dev)
+	})
+
+	var idToDelete int
+
+	for _, res2 := range res {
+		if res2.Charger == config.NameForID(id) {
+			idToDelete = res2.ID
+		}
+	}
+
+	if err := deleteDevice(idToDelete, h); err != nil {
+		return err
+	}
+	setConfigDirty()
+	return nil
+}
